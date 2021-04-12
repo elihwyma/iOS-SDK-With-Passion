deb08dc334e09d8bdba8edbcbcc5fe87f85d9382
Amy While <26681721+elihwyma@users.noreply.github.com>
2021-04-12 17:17:24 +0100

    Fucking Capt

---
+++ iPhoneOS14.4-Xcode.sdk/System/Library/Frameworks/NearField.framework/Headers/NFHardwareManagerCallbacks.h
@@ -0,0 +1,14 @@
+//
+//  NFHardwareManagerCallbacks.h
+//  Aemulo
+//
+//  Created by Amy While on 13/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NFHardwareManagerCallbacks : NSObject
+
++ (id)interface;
+
+@end
